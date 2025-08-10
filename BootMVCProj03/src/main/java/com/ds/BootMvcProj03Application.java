bd1ca426ce53108fe61f490ebbb4bc394d8fe898
Darpahari66 <mca.darpahari@gmail.com>
2025-08-10 22:07:26 +0530

    mvc project

---
+++ BootMVCProj03/src/main/java/com/ds/BootMvcProj03Application.java
@@ -0,0 +1,13 @@
+package com.ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcProj03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcProj03Application.class, args);
+	}
+
+}
